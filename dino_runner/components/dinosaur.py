5a42f24011d528728ca8d5dc40e0777d26c275e7
Kesse <64742407+KetCode@users.noreply.github.com>
2023-05-19 11:53:08 -0300

    added run function

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,4 +1,9 @@
 import pygame
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
+
+X_POS = 80
+Y_POS = 310
+JUMP_VEL = 8.5
 
 class Dinosaur:
     def __init__(self):
@@ -12,3 +17,13 @@ class Dinosaur:
         self.dino_duck = False
         self.step_index = 0
         self.jump_vel = JUMP_VEL
+
+    def run(self):
+        self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
+        self.dino_rect = self.image.get_rect() # atualiza o x e y (altura e largura do dino)
+        self.dino_rect.x = X_POS
+        self.dino_rect.y = Y_POS
+        self.step_index += 1
+
+        if self.step_index >= 10:
+            self.step_index = 0
